cf69cab9e1a63ba3eab1e9147ee6ad6f1c186ba6
Dariia Shevchuk <dashamensha@gmail.com>
2023-07-20 01:33:40 +0200

    All added

--- models/hotels.js
+++ models/hotels.js
@@ -1,29 +1,66 @@
-const { Sequelize, DataTypes } = require('sequelize');
+// const { Sequelize, DataTypes } = require('sequelize');
 
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: './databases/booking.db',
+// });
+
+// const Hotel = sequelize.define('Hotel', {
+//   hotel_id: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   name: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   city: {
+//     type: DataTypes.STRING,
+//     allowNull: false,
+//   },
+//   stars: {
+//     type: DataTypes.INTEGER,
+//     allowNull: false,
+//   },
+// });
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+// const sequelize = require('../database');
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './databases/booking.db',
-});
+    dialect: 'sqlite',
+    storage: './databases/booking.db',
+  });
+class Hotel extends Model {}
 
-const Hotel = sequelize.define('Hotel', {
-  hotel_id: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  city: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  stars: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
+Hotel.init(
+  {
+    // Define the attributes of the Hotel model
+    hotel_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    stars: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            },
+    },
+    {
+        sequelize,
+        modelName: 'Hotel',
+        tableName: 'Hotels',
+    }
+);
 
+// module.exports = Hotel;
 // Create the table
 // (async () => {
 //   try {
